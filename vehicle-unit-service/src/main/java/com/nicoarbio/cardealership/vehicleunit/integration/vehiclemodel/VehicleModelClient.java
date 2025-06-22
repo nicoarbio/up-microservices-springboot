6415200764c5446753945397caa5b20be630422a
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-06-22 00:31:25 -0300

    sales-service: minor fix

--- vehicle-unit-service/src/main/java/com/nicoarbio/cardealership/vehicleunit/integration/vehiclemodel/VehicleModelClient.java
+++ vehicle-unit-service/src/main/java/com/nicoarbio/cardealership/vehicleunit/integration/vehiclemodel/VehicleModelClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "vehicle-model-service", fallbackFactory = VehicleModelClientFallbackFactory.class)
+@FeignClient("vehicle-model-service")
 public interface VehicleModelClient {
 
     @GetMapping("/api/v1/vehicle-models/{id}")
